7a92f13857e6b7827b1612ac16cddba4cb66bb8d
THEAdnanC <adnandidthis@gmail.com>
2018-02-09 18:03:14 -0600

    new deploy

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, './dist'),
-        publicPath: '/dist/',
+        publicPath: '/test/dist/',
         filename: 'build.js'
     },
     resolve: {
